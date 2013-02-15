2bd122192886a73f7157d549186bf62e5761d3ff
Meriem Selmi <Meriem@satellite>
2013-02-15 10:40:42 +0100

    Bluetooth working

--- AndroCope/src/tn/insat/androcope/MainActivity.java
+++ AndroCope/src/tn/insat/androcope/MainActivity.java
@@ -2,21 +2,50 @@ package tn.insat.androcope;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
+		
+	private final static int REQUEST_CODE_ENABLE_BLUETOOTH = 0;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+		
+		BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (bluetoothAdapter == null)
+		   Toast.makeText(MainActivity.this, "Pas de Bluetooth", 
+		                  Toast.LENGTH_SHORT).show();
+		else
+		   Toast.makeText(MainActivity.this, "Avec Bluetooth", 
+		                  Toast.LENGTH_SHORT).show();
 
+		if (!bluetoothAdapter.isEnabled()) {
+		   Intent enableBlueTooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		   startActivityForResult(enableBlueTooth, REQUEST_CODE_ENABLE_BLUETOOTH);
+		}
+		else{
+			Toast.makeText(MainActivity.this, "Bluetooth already activated", 
+	                  Toast.LENGTH_SHORT).show();
+		}
+	}
+	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	       super.onActivityResult(requestCode, resultCode, data);
+	       if (requestCode != REQUEST_CODE_ENABLE_BLUETOOTH)
+	           return;
+	       if (resultCode == RESULT_OK) {
+	    	   Toast.makeText(MainActivity.this, "Activated", 
+		                  Toast.LENGTH_SHORT).show();
+	       } else {
+	    	   Toast.makeText(MainActivity.this, "Not Activated", 
+		                  Toast.LENGTH_SHORT).show();
+	       }    
 	}
 
 }
