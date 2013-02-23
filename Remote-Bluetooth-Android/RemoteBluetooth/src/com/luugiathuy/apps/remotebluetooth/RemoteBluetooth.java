650a69103c45a4308eb02e87ef3b1532d06d9be0
Meriem Selmi <Meriem@satellite>
2013-02-23 10:20:24 +0100

    Created class Handler out of the RemoteBluetooth class

--- Remote-Bluetooth-Android/RemoteBluetooth/src/com/luugiathuy/apps/remotebluetooth/RemoteBluetooth.java
+++ Remote-Bluetooth-Android/RemoteBluetooth/src/com/luugiathuy/apps/remotebluetooth/RemoteBluetooth.java
@@ -47,6 +47,8 @@ public class RemoteBluetooth extends Activity implements OnGestureListener{
 	private BluetoothCommandService mCommandService = null;
 	private GestureDetector gestureDetector;
 
+	private Handler mHandler;
+	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -63,7 +65,7 @@ public class RemoteBluetooth extends Activity implements OnGestureListener{
 		mTitle = (TextView) findViewById(R.id.title_left_text);
 		mTitle.setText(R.string.app_name);
 		mTitle = (TextView) findViewById(R.id.title_right_text);
-
+		mHandler = new MyHandler(mTitle, getApplicationContext());
 		// Get local Bluetooth adapter
 		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
@@ -131,43 +133,7 @@ public class RemoteBluetooth extends Activity implements OnGestureListener{
 			startActivity(discoverableIntent);
 		}
 	}
-
-	// The Handler that gets information back from the BluetoothChatService
-	private final Handler mHandler = new Handler() {
-		@Override
-		public void handleMessage(Message msg) {
-			switch (msg.what) {
-			case MESSAGE_STATE_CHANGE:
-				switch (msg.arg1) {
-				case BluetoothCommandService.STATE_CONNECTED:
-					mTitle.setText(R.string.title_connected_to);
-					mTitle.append(mConnectedDeviceName);
-					break;
-				case BluetoothCommandService.STATE_CONNECTING:
-					mTitle.setText(R.string.title_connecting);
-					break;
-				case BluetoothCommandService.STATE_LISTEN:
-				case BluetoothCommandService.STATE_NONE:
-					mTitle.setText(R.string.title_not_connected);
-					break;
-				}
-				break;
-			case MESSAGE_DEVICE_NAME:
-				// save the connected device's name
-				mConnectedDeviceName = msg.getData().getString(DEVICE_NAME);
-				Toast.makeText(getApplicationContext(),
-						"Connected to " + mConnectedDeviceName,
-						Toast.LENGTH_SHORT).show();
-				break;
-			case MESSAGE_TOAST:
-				Toast.makeText(getApplicationContext(),
-						msg.getData().getString(TOAST), Toast.LENGTH_SHORT)
-						.show();
-				break;
-			}
-		}
-	};
-
+	
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		switch (requestCode) {
 		case REQUEST_CONNECT_DEVICE:
