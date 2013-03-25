40ccde3dcebe53b3e1b41164b805d6acd8f7cddb
Meriem Selmi <Meriem@satellite>
2013-03-25 09:20:54 +0100

    Added clipboard to Mouse

--- AndroCope/src/tn/insat/androcope/Mouse.java
+++ AndroCope/src/tn/insat/androcope/Mouse.java
@@ -8,16 +8,24 @@ public class Mouse implements Serializable {
 	public static final int SCROLL = 0;
 	public static final int RIGHT_CLICK = 1;
 	public static final int LEFT_CLICK = 2;
+	public static final int ACTION_COPY = 3;
+	public static final int ACTION_PASTE = 4;
 	public static final int EXIT_CMD = -1;
 	
 	int action;
 	int x;
 	int y;
+	String clipboard;
 	
 	public Mouse(int action, int x, int y){
 		this.action=action;
 		this.x=x;
 		this.y=y;
+	}
+	
+	public Mouse(int action, String clipboard){
+		this.action=action;
+		this.clipboard = clipboard;
 	}
 
 	public int getAction() {
@@ -43,6 +51,15 @@ public class Mouse implements Serializable {
 	public void setY(int y) {
 		this.y = y;
 	}
+
+	public String getClipboard() {
+		return clipboard;
+	}
+
+	public void setClipboard(String clipboard) {
+		this.clipboard = clipboard;
+	}
+	
 	
 	
 }
