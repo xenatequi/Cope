b98d3d994c64e05743c0446131023a7e4daa8f53
miminf <mariem@vaio.com>
2013-03-27 16:56:35 +0100

    Push Partie Serveur Lundi

--- AndroCope/gen/tn/insat/androcope/R.java
+++ AndroCope/gen/tn/insat/androcope/R.java
@@ -10,20 +10,31 @@ package tn.insat.androcope;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
+         */
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int button_scan=0x7f060006;
-        public static final int discoverable=0x7f060009;
-        public static final int layout=0x7f060007;
-        public static final int new_devices=0x7f060005;
-        public static final int paired_devices=0x7f060003;
-        public static final int scan=0x7f060008;
-        public static final int title_left_text=0x7f060000;
-        public static final int title_new_devices=0x7f060004;
-        public static final int title_paired_devices=0x7f060002;
-        public static final int title_right_text=0x7f060001;
+        public static final int action_settings=0x7f07000a;
+        public static final int button_scan=0x7f070006;
+        public static final int discoverable=0x7f070009;
+        public static final int layout=0x7f070007;
+        public static final int new_devices=0x7f070005;
+        public static final int paired_devices=0x7f070003;
+        public static final int scan=0x7f070008;
+        public static final int title_left_text=0x7f070000;
+        public static final int title_new_devices=0x7f070004;
+        public static final int title_paired_devices=0x7f070002;
+        public static final int title_right_text=0x7f070001;
     }
     public static final class layout {
         public static final int custom_title=0x7f030000;
@@ -32,24 +43,28 @@ public final class R {
         public static final int main=0x7f030003;
     }
     public static final class menu {
-        public static final int option_menu=0x7f050000;
+        public static final int option_menu=0x7f060000;
+        public static final int splash=0x7f060001;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int bt_not_enabled_leaving=0x7f040003;
-        public static final int btn_find=0x7f040002;
-        public static final int button_scan=0x7f040006;
-        public static final int connect=0x7f04000b;
-        public static final int discoverable=0x7f04000c;
-        public static final int hello=0x7f040000;
-        public static final int none_found=0x7f040009;
-        public static final int none_paired=0x7f040007;
-        public static final int scanning=0x7f040008;
-        public static final int select_device=0x7f04000a;
-        public static final int title_connected_to=0x7f04000d;
-        public static final int title_connecting=0x7f04000e;
-        public static final int title_not_connected=0x7f04000f;
-        public static final int title_other_devices=0x7f040005;
-        public static final int title_paired_devices=0x7f040004;
+        public static final int action_settings=0x7f050011;
+        public static final int app_name=0x7f050001;
+        public static final int bt_not_enabled_leaving=0x7f050003;
+        public static final int btn_find=0x7f050002;
+        public static final int button_scan=0x7f050006;
+        public static final int connect=0x7f05000b;
+        public static final int discoverable=0x7f05000c;
+        public static final int hello=0x7f050000;
+        public static final int hello_world=0x7f050012;
+        public static final int none_found=0x7f050009;
+        public static final int none_paired=0x7f050007;
+        public static final int scanning=0x7f050008;
+        public static final int select_device=0x7f05000a;
+        public static final int title_activity_splash=0x7f050010;
+        public static final int title_connected_to=0x7f05000d;
+        public static final int title_connecting=0x7f05000e;
+        public static final int title_not_connected=0x7f05000f;
+        public static final int title_other_devices=0x7f050005;
+        public static final int title_paired_devices=0x7f050004;
     }
 }
